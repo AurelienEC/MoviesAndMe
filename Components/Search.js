7d0acb0cd482dd819905275696980a6fcbd713ee
Aurélien Antonio <aurelienantonio@MacBook-Pro-de-Aurelien.local>
2018-10-06 22:31:38 -0500

    Step 2: Adding Style

--- Components/Search.js
+++ Components/Search.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import {View, Button, TextInput} from 'react-native'
+import {StyleSheet, View, Button, TextInput} from 'react-native'
 
 class Search extends React.Component {
   render(){
     return(
-      <View>
-        <TextInput placeholder="Titre du Film"/>
+      <View style={{marginTop:20}}>
+        <TextInput style={styles.textinput} placeholder="Titre du Film"/>
         <Button title="Rechercher" onPress={()=>{}} />
       </View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  textinput : {
+    marginLeft: 5,
+    marginRight:5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth:1,
+    paddingLeft:5
+  }
+})
 export default Search
