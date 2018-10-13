1b550ab0afb7db0855ce46fef5889ec1703e4040
Aurélien Antonio <aurelienantonio@MacBook-Pro-de-Aurelien.local>
2018-10-13 07:48:11 -0500

    Step 9a: Components LifeCycle - Before Adding ScrollView  1. Creating a function to get movie details from the databas   2. Implementing ComponentDidMount Method

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,21 +1,60 @@
+
+//imports
 import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import {StyleSheet, View, ActivityIndicator} from 'react-native'
+import {getFilmDetailFromApi} from '../API/TMDBApi'
 
 class FilmDetail extends React.Component {
+  //supercharging Methods
+  constructor(props){
+    this.state = {
+      film: undefined,
+      isLoading: true
+    }
+  }
+  componentDidMount(){
+    getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+      this.setState({
+        film: data,
+        isLoading: false
+      })
+    })
+
+  }
+//internal Methods
+  _displayLoading(){
+    if(this.state.isLoading){
+      return(
+        <View style={styles.loading_container}>
+            <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+//rendering
   render() {
-    const idFilm = this.props.navigation.state.params.idFilm
+
     return (
       <View style={styles.main_container}>
-        <Text>Détail du film {idFilm}</Text>
+        {this._displayLoading()}
       </View>
     )
   }
 }
-
+//StyleSheet
 const styles= StyleSheet.create({
   main_container:{
     flex:1,
-  }
+  },
+  loading_container: {
+   position: 'absolute',
+   left: 0,
+   right: 0,
+   top: 0,
+   bottom: 0,
+   alignItems: 'center',
+   justifyContent: 'center'
+ }
 })
 
 export default FilmDetail
