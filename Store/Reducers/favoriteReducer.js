e97b0760ba37885646c0830c84c8664bbe8ec6c0
Aurélien Antonio <aurelienantonio@MacBook-Pro-de-Aurelien.local>
2018-11-28 15:09:16 -0500

    step 10 : Redux Reducer and Store

---
+++ Store/Reducers/favoriteReducer.js
@@ -0,0 +1,23 @@
+const initialState = {favoritesFilm: [], title:"Mes films favoris"}
+function toggleFavorite(state = initialState, action){
+    let nextState
+    switch (action.type) {
+        case 'TOGGLE_FAVORITE':
+            const favoriteFilmIndex = state.favoritesFilm.findIndex(item => item.id === action.value.id)
+            if (favoriteFilmIndex !== -1){
+                nextState = {
+                    ...state,
+                    favoritesFilm: state.favoritesFilm.filter((item, index) => index !== favoriteFilmIndex)
+                }
+            }else{
+               nextState = {
+                   ...state,
+                   favoritesFilm: [...state.favoritesFilm, action.value]
+               }
+            }
+            return nextState || state
+        default:
+            return state
+    }
+}
+export default toggleFavorite
