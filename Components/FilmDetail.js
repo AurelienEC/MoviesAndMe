3ca0ac7e7240cd959735a9d39a7a4c736b829556
Aurélien Antonio <aurelienantonio@MacBook-Pro-de-Aurelien.local>
2018-11-28 15:51:20 -0500

    Step 11: Create an Action for Redux

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,7 +1,7 @@
 // Components/FilmDetail.js
 
 import React from 'react'
-import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image } from 'react-native'
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image, TouchableOpacity } from 'react-native'
 import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBApi'
 import moment from 'moment'
 import numeral from 'numeral'
@@ -34,7 +34,24 @@ class FilmDetail extends React.Component {
             )
         }
     }
-
+    _toggleFavorite(){
+        const action = { type: "TOGGLE_FAVORITE", value: this.state.film }
+        this.props.dispatch(action)
+    }
+    componentDidUpdate(){
+        console.log(this.props.favoritesFilm)
+    }
+    _displayFavoriteImage(){
+        var sourceImage = require('../Images/ic_favorite_border.png')
+        if(this.props.favoritesFilm.findIndex(item => item.id === this.state.film.id) !== -1 ){
+            sourceImage = require('../Images/ic_favorite.png')
+        }
+        return (
+            <Image
+            source={sourceImage}
+            style={styles.favorite_image}/>
+        )
+    }
     _displayFilm() {
         const { film } = this.state
         if (film != undefined) {
@@ -45,6 +62,11 @@ class FilmDetail extends React.Component {
                         source={{uri: getImageFromApi(film.backdrop_path)}}
                     />
                     <Text style={styles.title_text}>{film.title}</Text>
+                    <TouchableOpacity
+                        onPress={() => this._toggleFavorite()}
+                        style={styles.favorite_container}>
+                        {this._displayFavoriteImage()}
+                    </TouchableOpacity>
                     <Text style={styles.description_text}>{film.overview}</Text>
                     <Text style={styles.default_text}>Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
                     <Text style={styles.default_text}>Note : {film.vote_average} / 10</Text>
@@ -64,7 +86,6 @@ class FilmDetail extends React.Component {
     }
 
     render() {
-        console.log(this.props)
         return (
             <View style={styles.main_container}>
                 {this._displayLoading()}
@@ -116,6 +137,13 @@ const styles = StyleSheet.create({
         marginLeft: 5,
         marginRight: 5,
         marginTop: 5,
+    },
+    favorite_container: {
+        alignItems: "center"
+    },
+    favorite_image:{
+        width: 40,
+        height: 40
     }
 })
 const mapStateToProps = (state) => {
